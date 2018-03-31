a88be11068adc7fc5b6051f5e17889d880840b85
ivanoung <ivanoung@gmail.com>
2018-03-31 11:48:57 +0800

    testing out withdb layout

--- server.js
+++ server.js
@@ -11,66 +11,35 @@ var __importStar = (this && this.__importStar) || function (mod) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const multer_1 = __importDefault(require("multer"));
-const path = __importStar(require("path"));
+const fs = __importStar(require("fs"));
 // import fileUpload from 'express-fileupload';
-// Set storage engine
-const storage = multer_1.default.diskStorage({
-    destination: './public/uploads/',
-    filename: function (req, file, cb) {
-        cb(null, `${file.filename}-${Date.now()}${path.extname(file.originalname)}`);
-    }
-});
-// Init upload
-const upload = multer_1.default({
-    storage: storage
-}).single('myImage');
 const app = express_1.default();
-// app.use(fileUpload());
-// Setup EJS
-app.set('view engine', 'ejs');
-// Public Folder
-app.use(express_1.default.static('./public'));
-app.get('/', (req, res) => { res.render('index'); });
-app.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-        if (err) {
-            res.render('index', {
-                msg: err
-            });
-        }
-        else {
-            console.log(req.file);
-            res.send('test');
+app.set("view engine", "ejs");
+// app.get('/',(req,res)=>{
+//     console.log(req.body);
+//     let index = fs.readFile('index.html', 'utf8',(err,data)=>{
+//         res.send(data);
+//     })
+// })
+// app.get('/',(req,res)=>{
+//     const list = fs.readdir('./public/uploads','buffer', (err, listOfNames)=>{
+//         if(err){
+//             console.log("there is no file in folder");
+//         }else{
+//             // res.send(listOfNames);
+//         }
+//     })
+//     // res.render('index');
+// })
+// app.use(express.static("public"));
+app.get("/", (req, res, next) => {
+    next();
+});
+app.get("/", (req, res) => {
+    let list = fs.readdir("./public/uploads", "utf8", (err, listofnames) => {
+        if (!err) {
+            res.render("upload", { data: listofnames });
         }
     });
 });
-app.listen(8080, () => console.log(`Server started on port 8080`));
-// ------------------------------------------------------------------------------------------------------------------------
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// })
-// app.post('/upload-file', (req, res) => {
-//     // req.files.upload
-//     // let ts = (req.files) ? fs.writeFile(req.files.upload.name, req.files.upload.data, (err) => console.log(err)) : null;
-//     if (req.files) { console.log(req.files.upload); }
-//     else { res.status(400).send("NOTHIING UPLOADED"); }
-// })
-// app.post('/upload', function (req, res) {
-//     if (!req.files)
-//         return res.status(400).send('No files were uploaded.');
-//     else {
-//         let sampleFile = req.files.sampleFile;
-//         if (! (sampleFile instanceof Array) ) {
-//             fs.writeFile('test.jpg', sampleFile.data, (err) => { console.log(err) })
-//         }
-//         // res.send("did go through");
-//     }
-// The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-// Use the mv() method to place the file somewhere on your server
-// sampleFile.mv('/somewhere/on/your/server/filename.jpg', function (err) {
-//     if (err)
-//         return res.status(500).send(err);
-//     res.send('File uploaded!');
-// });
-// });
+app.listen(8080);
--- server.ts
+++ server.ts
@@ -1,93 +1,27 @@
-import express from 'express';
-import multer from 'multer';
-import * as ejs from 'ejs';
-import * as fs from 'fs';
+import express from "express";
+import multer from "multer";
+import * as ejs from "ejs";
+import * as fs from "fs";
 import * as path from "path";
-import * as bodyParser from 'body-parser';
+import * as bodyParser from "body-parser";
+import { serveStatic } from "serve-static";
 // import fileUpload from 'express-fileupload';
 
-// Set storage engine
-const storage = multer.diskStorage({
-    destination: './public/uploads/',
-    filename: function(req, file, cb){
-        cb(null, `${file.filename}-${Date.now()}${path.extname(file.originalname)}`)
-    }
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res, next) => {
+  next();
 });
 
-// Init upload
-const upload = multer({
-    storage: storage
-}).single('myImage');
+app.get("/", (req, res) => {
+  let list = fs.readdir("./public/uploads", "utf8", (err, listofnames) => {
+    if (!err) {
+      res.render("upload", { data: listofnames });
+    }
+  });
+});
 
-const app = express();
-// app.use(fileUpload());
-
-
-// Setup EJS
-app.set('view engine', 'ejs');
-
-// Public Folder
-app.use(express.static('./public'));
-
-app.get('/', (req,res)=>{res.render('index')})
-
-app.post('/upload', (req, res)=>{
-    upload(req, res,(err)=>{
-        if (err){
-            res.render('index', {
-                msg: err
-            });
-        }else {
-            console.log(req.file);
-            res.send('test');
-        }
-    })
-})
-
-app.listen(8080, ()=> console.log(`Server started on port 8080`));
-
-
-
-
-
-// ------------------------------------------------------------------------------------------------------------------------
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// })
-
-// app.post('/upload-file', (req, res) => {
-
-//     // req.files.upload
-//     // let ts = (req.files) ? fs.writeFile(req.files.upload.name, req.files.upload.data, (err) => console.log(err)) : null;
-
-//     if (req.files) { console.log(req.files.upload); }
-//     else { res.status(400).send("NOTHIING UPLOADED"); }
-
-// })
-
-// app.post('/upload', function (req, res) {
-//     if (!req.files)
-//         return res.status(400).send('No files were uploaded.');
-//     else {
-//         let sampleFile = req.files.sampleFile;
-//         if (! (sampleFile instanceof Array) ) {
-//             fs.writeFile('test.jpg', sampleFile.data, (err) => { console.log(err) })
-//         }
-
-//         // res.send("did go through");
-//     }
-
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-
-
-    // Use the mv() method to place the file somewhere on your server
-    // sampleFile.mv('/somewhere/on/your/server/filename.jpg', function (err) {
-    //     if (err)
-    //         return res.status(500).send(err);
-
-    //     res.send('File uploaded!');
-    // });
-// });
-
-
-
+app.listen(8080);

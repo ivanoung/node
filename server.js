5a98daa57e3091c6144250ec2ac5669f01efce11
ivanoung <ivanoung@gmail.com>
2018-04-02 22:18:24 +0800

    more test

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function(mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-var __importStar =
-  (this && this.__importStar) ||
-  function(mod) {
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+}
+var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null)
-      for (var k in mod)
-        if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
     result["default"] = mod;
     return result;
-  };
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const fs = __importStar(require("fs"));
@@ -30,35 +24,54 @@ app.set("views", path.join(__dirname, "/public/views"));
 app.use(express_1.default.static(path.join(__dirname, "/public")));
 // Setting homepage
 app.get("/", (req, res) => {
-  let list = fs.readdir(
-    path.join(__dirname, "/public/uploads"),
-    "utf8",
-    (err, listofnames) => {
-      if (!err) {
-        res.render("upload", { data: listofnames });
-      } else {
-        res.render("upload", { data: ["Threre is nothing inside the list"] });
-      }
-    }
-  );
+    // let stat = fs.stat('./public/uploads/hotchick.jpg',(err, stats)=>{
+    //   console.log(stats.mtime);
+    // })
+    let list = fs.readdir(path.join(__dirname, "/public/uploads"), "utf8", (err, listofnames) => {
+        if (!err) {
+            console.log(listofnames);
+            res.render("upload", { data: listofnames });
+        }
+        else {
+            res.render("upload", { data: ["Threre is nothing inside the list"] });
+        }
+    });
 });
 // Routing for uploading file/folder, one by one
 app.post("/upload", (req, res) => {
-  console.log(req.files);
-  if (JSON.stringify(req.files) == "{}") {
-    res.status(400).end("No files were uploaded");
-  } else {
-    let saveFile = req.files.uploadFile;
-
-    saveFile.mv('/public/uploads/something.jpg',(err)=>{
-        if (err){
-            res.status(500).end(err);
-        }
-        else{
-            res.send("Operation Done");
-        }
-    })
-  }
+    // console.log(req.files);
+    let inputNameField = "uploadFile";
+    if (JSON.stringify(req.files) == "{}") {
+        res.status(400).end("No files were uploaded");
+    }
+    else if (req.files) {
+        let file;
+        file = req.files[inputNameField];
+        console.log(file);
+        fs.writeFile(`${__dirname}/public/uploads/${file["name"]}`, file.data, err => {
+            if (!err) {
+                // Try
+                let list = fs.readdir(path.join(__dirname, "/public/uploads"), "utf8", (err, listofnames) => {
+                    if (!err) {
+                        res.render("upload", { data: listofnames });
+                    }
+                    else {
+                        res.render("upload", {
+                            data: ["Threre is nothing inside the list"]
+                        });
+                    }
+                });
+                // End of try
+            }
+            else {
+                res.send("error");
+            }
+        });
+    }
+});
+app.get('/uploads/:filename', (req, res) => {
+    console.log(req.params.filename);
+    res.download(req.params.filename);
 });
 app.listen(8080, () => console.log(`Server running on port 8080`));
 // Set Storage Engine
@@ -72,3 +85,13 @@ app.listen(8080, () => console.log(`Server running on port 8080`));
 // const upload = multer({
 //     storage: storage
 // }).single('uploadFile');
+// (req.files instanceof fileUpload.FileArray && req.files.uploadFile instanceof fileUpload.FileArray) {
+//   let saveFile = req.files.uploadFile;
+//   saveFile.mv(`./public/uploads/${req.files.name}`, err => {
+//     if (err) {
+//       res.status(500).end(err);
+//     } else {
+//       res.send("Operation Done");
+//     }
+//   });
+// }
--- server.ts
+++ server.ts
@@ -20,11 +20,16 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 // Setting homepage
 app.get("/", (req, res) => {
+  // let stat = fs.stat('./public/uploads/hotchick.jpg',(err, stats)=>{
+  //   console.log(stats.mtime);
+  // })
+
   let list = fs.readdir(
     path.join(__dirname, "/public/uploads"),
     "utf8",
     (err, listofnames) => {
       if (!err) {
+        console.log(listofnames);
         res.render("upload", { data: listofnames });
       } else {
         res.render("upload", { data: ["Threre is nothing inside the list"] });
@@ -35,21 +40,47 @@ app.get("/", (req, res) => {
 
 // Routing for uploading file/folder, one by one
 app.post("/upload", (req, res) => {
-  console.log(req.files);
+  // console.log(req.files);
+  let inputNameField = "uploadFile";
 
   if (JSON.stringify(req.files) == "{}") {
     res.status(400).end("No files were uploaded");
-  } else if (req.files instanceof fileUpload.FileArray && req.files.uploadFile instanceof fileUpload.FileArray) {
-    let saveFile = req.files.uploadFile;
-    saveFile.mv(`./public/uploads/${req.files.name}`, err => {
-      if (err) {
-        res.status(500).end(err);
-      } else {
-        res.send("Operation Done");
+  } else if (req.files) {
+    let file: any;
+    file = req.files[inputNameField];
+    console.log(file);
+    fs.writeFile(
+      `${__dirname}/public/uploads/${file["name"]}`,
+      file.data,
+      err => {
+        if (!err) {
+          // Try
+          let list = fs.readdir(
+            path.join(__dirname, "/public/uploads"),
+            "utf8",
+            (err, listofnames) => {
+              if (!err) {
+                res.render("upload", { data: listofnames });
+              } else {
+                res.render("upload", {
+                  data: ["Threre is nothing inside the list"]
+                });
+              }
+            }
+          );
+          // End of try
+        } else {
+          res.send("error");
+        }
       }
-    });
+    );
   }
 });
+
+app.get('/uploads/:filename',(req, res)=>{
+  console.log(req.params.filename);
+  res.download(req.params.filename);
+})
 
 app.listen(8080, () => console.log(`Server running on port 8080`));
 
@@ -65,3 +96,14 @@ app.listen(8080, () => console.log(`Server running on port 8080`));
 // const upload = multer({
 //     storage: storage
 // }).single('uploadFile');
+
+// (req.files instanceof fileUpload.FileArray && req.files.uploadFile instanceof fileUpload.FileArray) {
+//   let saveFile = req.files.uploadFile;
+//   saveFile.mv(`./public/uploads/${req.files.name}`, err => {
+//     if (err) {
+//       res.status(500).end(err);
+//     } else {
+//       res.send("Operation Done");
+//     }
+//   });
+// }

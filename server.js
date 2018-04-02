20acadcc415859ae3a19e67e9fe375266d67a55a
ivanoung <ivanoung@gmail.com>
2018-04-02 16:47:01 +0800

    No Multer yes Express-upload

--- server.js
+++ server.js
@@ -1,21 +1,74 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-}
+var __importDefault =
+  (this && this.__importDefault) ||
+  function(mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+var __importStar =
+  (this && this.__importStar) ||
+  function(mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null)
+      for (var k in mod)
+        if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-// import fileUpload from 'express-fileupload';
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+const express_fileupload_1 = __importDefault(require("express-fileupload"));
+// Init express
 const app = express_1.default();
+app.use(express_fileupload_1.default());
+// Setting views, and pathing for views
 app.set("view engine", "ejs");
-app.use(express_1.default.static("./public"));
-// app.get("/", (req, res, next) => {
-//   next();
-// });
-// app.get("/", (req, res) => {
-//   let list = fs.readdir("./public/uploads", "utf8", (err, listofnames) => {
-//     if (!err) {
-//       res.render("upload", { data: listofnames });
+app.set("views", path.join(__dirname, "/public/views"));
+// Serving static page
+app.use(express_1.default.static(path.join(__dirname, "/public")));
+// Setting homepage
+app.get("/", (req, res) => {
+  let list = fs.readdir(
+    path.join(__dirname, "/public/uploads"),
+    "utf8",
+    (err, listofnames) => {
+      if (!err) {
+        res.render("upload", { data: listofnames });
+      } else {
+        res.render("upload", { data: ["Threre is nothing inside the list"] });
+      }
+    }
+  );
+});
+// Routing for uploading file/folder, one by one
+app.post("/upload", (req, res) => {
+  console.log(req.files);
+  if (JSON.stringify(req.files) == "{}") {
+    res.status(400).end("No files were uploaded");
+  } else {
+    let saveFile = req.files.uploadFile;
+
+    saveFile.mv('/public/uploads/something.jpg',(err)=>{
+        if (err){
+            res.status(500).end(err);
+        }
+        else{
+            res.send("Operation Done");
+        }
+    })
+  }
+});
+app.listen(8080, () => console.log(`Server running on port 8080`));
+// Set Storage Engine
+// const storage = multer.diskStorage({
+//     destination: './public/uploads/',
+//     filename: function(req, file, callback){
+//         callback(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
 //     }
-//   });
 // });
-app.listen(8080);
+// Init upload variable
+// const upload = multer({
+//     storage: storage
+// }).single('uploadFile');
--- server.ts
+++ server.ts
@@ -1,30 +1,67 @@
 import express from "express";
-import multer from "multer";
+// import multer from "multer";
 import * as ejs from "ejs";
 import * as fs from "fs";
 import * as path from "path";
 import * as bodyParser from "body-parser";
 import { serveStatic } from "serve-static";
-// import fileUpload from 'express-fileupload';
+import fileUpload from "express-fileupload";
 
+// Init express
 const app = express();
+app.use(fileUpload());
 
+// Setting views, and pathing for views
 app.set("view engine", "ejs");
-app.use(express.static("./public"));
+app.set("views", path.join(__dirname, "/public/views"));
 
-// app.get("/", (req, res, next) => {
-//   next();
-// });
+// Serving static page
+app.use(express.static(path.join(__dirname, "/public")));
 
-// app.get("/", (req, res) => {
-//   let list = fs.readdir("./public/uploads", "utf8", (err, listofnames) => {
-//     if (!err) {
-//       res.render("upload", { data: listofnames });
+// Setting homepage
+app.get("/", (req, res) => {
+  let list = fs.readdir(
+    path.join(__dirname, "/public/uploads"),
+    "utf8",
+    (err, listofnames) => {
+      if (!err) {
+        res.render("upload", { data: listofnames });
+      } else {
+        res.render("upload", { data: ["Threre is nothing inside the list"] });
+      }
+    }
+  );
+});
+
+// Routing for uploading file/folder, one by one
+app.post("/upload", (req, res) => {
+  console.log(req.files);
+
+  if (JSON.stringify(req.files) == "{}") {
+    res.status(400).end("No files were uploaded");
+  } else if (req.files instanceof fileUpload.FileArray && req.files.uploadFile instanceof fileUpload.FileArray) {
+    let saveFile = req.files.uploadFile;
+    saveFile.mv(`./public/uploads/${req.files.name}`, err => {
+      if (err) {
+        res.status(500).end(err);
+      } else {
+        res.send("Operation Done");
+      }
+    });
+  }
+});
+
+app.listen(8080, () => console.log(`Server running on port 8080`));
+
+// Set Storage Engine
+// const storage = multer.diskStorage({
+//     destination: './public/uploads/',
+//     filename: function(req, file, callback){
+//         callback(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
 //     }
-//   });
 // });
 
-
-// app.get('/upload')
-
-app.listen(8080);
+// Init upload variable
+// const upload = multer({
+//     storage: storage
+// }).single('uploadFile');

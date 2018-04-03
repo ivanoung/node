04830a71317df951a957747a7115e8496db5bab9
Ivan Oung <ivanoung@gmail.com>
2018-04-03 08:54:51 +0800

    --save handlebar nodejs

--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
 import express from "express";
-// import multer from "multer";
 import * as ejs from "ejs";
 import * as fs from "fs";
 import * as path from "path";
 import * as bodyParser from "body-parser";
 import { serveStatic } from "serve-static";
 import fileUpload from "express-fileupload";
+// import multer from "multer";
+
 
 // Init express
 const app = express();
